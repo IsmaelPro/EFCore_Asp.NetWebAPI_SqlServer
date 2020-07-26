7a57c40dcd62021340f43fae14bda069b0bad37d
Ismael Santos <ismael496@yahoo.com.br>
2020-07-26 17:48:37 -0300

    Programa criado e feita primeira Migration

--- IFCore.WebApi/Data/HeroiContext.cs
+++ IFCore.WebApi/Data/HeroiContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using IFCore.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IFCore.WebApi.Data
 {
-    public class HeroiContext
+    public class HeroiContext : DbContext
     {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;User ID=sa;Initial Catalog=HeroApp;Data Source=ISMAEL-NOTE");
+        }
     }
 }
